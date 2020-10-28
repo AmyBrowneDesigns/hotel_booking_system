da4916b023ae159323a8920c5a110b01d83c1b7a
Alan Parley <alanparley@hotmail.com>
2020-10-28 13:51:53 +0000

    data rendering on components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,37 @@
 <template>
-  <h1>HIIII</h1>
+<main>
+  <h1>Hotel Code</h1>
+  <bookings-grid v-if="bookings" :bookings="bookings"></bookings-grid>
+  <!-- <ul>
+    <li v-for="(booking, index) in bookings" :key="index">{{ booking.name}}</li>
+  </ul> -->
+</main>
+
+
 </template>
 
 <script>
-
+import BookingService from './services/BookingService.js';
+import BookingsGrid from './components/BookingsGrid.vue';
 
 export default {
   name: 'App',
   components: {
-
+    "bookings-grid": BookingsGrid
+  },
+  data () {
+    return {
+      bookings: []
+    }
+  },
+  mounted() {
+    this.showBookings()
+  },
+  methods: {
+    showBookings() {
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings)
+    }
   }
 }
 </script>
---
+++ client/src/components/booking.vue
@@ -0,0 +1,18 @@
+<template>
+  <article v-if="booking">
+    <h2>{{ booking.name}}</h2>
+    <p>{{ booking.email }}</p>
+    <p>{{ booking.checkedin }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "booking",
+  props: ["booking"]
+}
+</script>
+
+<style>
+
+</style>
